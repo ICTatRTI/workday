c44d0f8c64f61d3cf08c98fbe6840f143b89b7a7
Adam Preston <adam.m.preston@gmail.com>
2016-04-07 08:44:58 -0600

    finished questions These will need to be split up in to weekday and weekend question, alos the account create and login task will be added (with integration to researchnet)

--- workaday/ViewController.swift
+++ workaday/ViewController.swift
@@ -25,7 +25,7 @@ class ViewController: UIViewController, ORKTaskViewControllerDelegate{
     override func viewDidAppear(animated: Bool) {
         
         
-        let task = ORKOrderedTask(identifier: "task", steps: [instructionStep,workdayOneStep, workdayTwoStep, workdayThreeStep, workdayFourStep])
+        let task = ORKOrderedTask(identifier: "task", steps: [instructionStep,workdayOneStep, workdayTwoStep, workdayThreeStep, workdayFourStep,summaryStep])
         
         let taskViewController = ORKTaskViewController(task: task, taskRunUUID: nil)
         taskViewController.delegate = self
@@ -33,13 +33,10 @@ class ViewController: UIViewController, ORKTaskViewControllerDelegate{
         presentViewController(taskViewController, animated: true, completion: nil)
     }
 
-    
-    
+
     func taskViewController(taskViewController: ORKTaskViewController,
                             didFinishWithReason reason: ORKTaskViewControllerFinishReason,
                                                 error: NSError?) {
-        
-        
         
         // Then, dismiss the task view controller.
         dismissViewControllerAnimated(true, completion: nil)
@@ -73,21 +70,26 @@ class ViewController: UIViewController, ORKTaskViewControllerDelegate{
     //How did you get to work today?
      private var workdayOneStep : ORKQuestionStep {
         
-        let textChoiceOneText = NSLocalizedString("Bad", comment: "Not good at all")
-        let textChoiceTwoText = NSLocalizedString("Fine", comment: "Whatever")
-        let textChoiceThreeText = NSLocalizedString("Fantastic", comment: "Really good, actually")
+        let textChoiceOneText = NSLocalizedString("Drove own car", comment: "Drove own car")
+        let textChoiceTwoText = NSLocalizedString("Biked", comment: "Biked")
+        let textChoiceThreeText = NSLocalizedString("Walked/ran", comment: "Walked/ran")
+        let textChoiceFourText = NSLocalizedString("Carpool/vanpool", comment: "Carpool/vanpool")
+        let textChoiceFiveText = NSLocalizedString("Did not commute", comment: "Did not commute")
+        
         
         // The text to display can be separate from the value coded for each choice:
         let textChoices = [
-            ORKTextChoice(text: textChoiceOneText, value: "bad"),
-            ORKTextChoice(text: textChoiceTwoText, value: "fine"),
-            ORKTextChoice(text: textChoiceThreeText, value: "good")
+            ORKTextChoice(text: textChoiceOneText, value: "drove"),
+            ORKTextChoice(text: textChoiceTwoText, value: "biked"),
+            ORKTextChoice(text: textChoiceThreeText, value: "walked"),
+            ORKTextChoice(text: textChoiceFourText, value: "carpooled"),
+            ORKTextChoice(text: textChoiceFiveText, value: "none"),
         ]
         
         
         let answerFormat = ORKAnswerFormat.choiceAnswerFormatWithStyle(.SingleChoice, textChoices: textChoices)
         
-        let questionStep = ORKQuestionStep(identifier: "how did you get to work", title: "How is your day going today?", answer: answerFormat)
+        let questionStep = ORKQuestionStep(identifier: "How did you get to work", title: "How did you get to work today?", answer: answerFormat)
         
         return questionStep
         
@@ -97,22 +99,39 @@ class ViewController: UIViewController, ORKTaskViewControllerDelegate{
     // How do you feel today?
     private var workdayTwoStep : ORKQuestionStep {
         
-        
-        let textChoiceOneText = NSLocalizedString("Bad", comment: "Not good at all")
-        let textChoiceTwoText = NSLocalizedString("Fine", comment: "Whatever")
-        let textChoiceThreeText = NSLocalizedString("Fantastic", comment: "Really good, actually")
+        let textChoiceOneText = NSLocalizedString("Complete elated, rapturous joy and soaring ecstasy", comment: "")
+        let textChoiceTwoText = NSLocalizedString("Very elated and in very high spirits. Tremendous delight and buoyancy", comment: "")
+        let textChoiceThreeText = NSLocalizedString("Elated and in high spirits", comment: "")
+        let textChoiceFourText = NSLocalizedString("Feeling very good and cheerful", comment: "")
+        let textChoiceFiveText = NSLocalizedString("Feeling pretty good, 'OK'", comment: "")
+        let textChoiceSixText = NSLocalizedString("Feeling a little bit low. Just so-so", comment: "")
+        let textChoiceSevenText = NSLocalizedString("Spirits low and somewhat 'blue'", comment: "")
+        let textChoiceEightText = NSLocalizedString("Depressed and feeling very low. Definitely 'blue'", comment: "")
+        let textChoiceNineText = NSLocalizedString("Tremendously depressed. Feeling terrible, really miserable, 'just awful'", comment: "")
+        let textChoiceTenText = NSLocalizedString("Utter depression and gloom. Completely down.", comment: "")
+        let textChoiceElevenText = NSLocalizedString("All is black and leaden. Wish it were all over", comment: "")
         
         // The text to display can be separate from the value coded for each choice:
         let textChoices = [
-            ORKTextChoice(text: textChoiceOneText, value: "bad"),
-            ORKTextChoice(text: textChoiceTwoText, value: "fine"),
-            ORKTextChoice(text: textChoiceThreeText, value: "good")
+            ORKTextChoice(text: textChoiceOneText, value: "10"),
+            ORKTextChoice(text: textChoiceTwoText, value: "9"),
+            ORKTextChoice(text: textChoiceThreeText, value: "8"),
+            ORKTextChoice(text: textChoiceFourText, value: "7"),
+            ORKTextChoice(text: textChoiceFiveText, value: "6"),
+            ORKTextChoice(text: textChoiceSixText, value: "5"),
+            ORKTextChoice(text: textChoiceSevenText, value: "4"),
+            ORKTextChoice(text: textChoiceEightText, value: "3"),
+            ORKTextChoice(text: textChoiceNineText, value: "2"),
+            ORKTextChoice(text: textChoiceTenText, value: "1"),
+            ORKTextChoice(text: textChoiceElevenText, value: "0")
         ]
         
         
         let answerFormat = ORKAnswerFormat.choiceAnswerFormatWithStyle(.SingleChoice, textChoices: textChoices)
         
-        let questionStep = ORKQuestionStep(identifier: "how do you feel today", title: "How is your day going today?", answer: answerFormat)
+        let questionStep = ORKQuestionStep(identifier: "how do you feel today", title: "How do you feel today?", answer: answerFormat)
+        
+
         
         return questionStep
 
@@ -126,21 +145,21 @@ class ViewController: UIViewController, ORKTaskViewControllerDelegate{
     private var workdayThreeStep : ORKQuestionStep {
         
         
-        let textChoiceOneText = NSLocalizedString("Bad", comment: "Not good at all")
-        let textChoiceTwoText = NSLocalizedString("Fine", comment: "Whatever")
-        let textChoiceThreeText = NSLocalizedString("Fantastic", comment: "Really good, actually")
+        let textChoiceOneText = NSLocalizedString("Vigorously active for at least 30 min", comment: "")
+        let textChoiceTwoText = NSLocalizedString("Moderately active", comment: "")
+        let textChoiceThreeText = NSLocalizedString("Not very active, sedentary activities preferred", comment: "")
         
         // The text to display can be separate from the value coded for each choice:
         let textChoices = [
-            ORKTextChoice(text: textChoiceOneText, value: "bad"),
-            ORKTextChoice(text: textChoiceTwoText, value: "fine"),
-            ORKTextChoice(text: textChoiceThreeText, value: "good")
+            ORKTextChoice(text: textChoiceOneText, value: "active"),
+            ORKTextChoice(text: textChoiceTwoText, value: "medium"),
+            ORKTextChoice(text: textChoiceThreeText, value: "not")
         ]
         
         
         let answerFormat = ORKAnswerFormat.choiceAnswerFormatWithStyle(.SingleChoice, textChoices: textChoices)
         
-        let questionStep = ORKQuestionStep(identifier: "activity level", title: "How is your day going today?", answer: answerFormat)
+        let questionStep = ORKQuestionStep(identifier: "activity level", title: "What best describes your activity level today?", answer: answerFormat)
         
         return questionStep
         
@@ -153,21 +172,39 @@ class ViewController: UIViewController, ORKTaskViewControllerDelegate{
     private var workdayFourStep : ORKQuestionStep {
         
         
-        let textChoiceOneText = NSLocalizedString("Bad", comment: "Not good at all")
-        let textChoiceTwoText = NSLocalizedString("Fine", comment: "Whatever")
-        let textChoiceThreeText = NSLocalizedString("Fantastic", comment: "Really good, actually")
+        let textChoiceOneText = NSLocalizedString("Complete elated, rapturous joy and soaring ecstasy", comment: "")
+        let textChoiceTwoText = NSLocalizedString("Very elated and in very high spirits. Tremendous delight and buoyancy", comment: "")
+        let textChoiceThreeText = NSLocalizedString("Elated and in high spirits", comment: "")
+        let textChoiceFourText = NSLocalizedString("Feeling very good and cheerful", comment: "")
+        let textChoiceFiveText = NSLocalizedString("Feeling pretty good, 'OK'", comment: "")
+        let textChoiceSixText = NSLocalizedString("Feeling a little bit low. Just so-so", comment: "")
+        let textChoiceSevenText = NSLocalizedString("Spirits low and somewhat 'blue'", comment: "")
+        let textChoiceEightText = NSLocalizedString("Depressed and feeling very low. Definitely 'blue'", comment: "")
+        let textChoiceNineText = NSLocalizedString("Tremendously depressed. Feeling terrible, really miserable, 'just awful'", comment: "")
+        let textChoiceTenText = NSLocalizedString("Utter depression and gloom. Completely down.", comment: "")
+        let textChoiceElevenText = NSLocalizedString("All is black and leaden. Wish it were all over", comment: "")
         
         // The text to display can be separate from the value coded for each choice:
         let textChoices = [
-            ORKTextChoice(text: textChoiceOneText, value: "bad"),
-            ORKTextChoice(text: textChoiceTwoText, value: "fine"),
-            ORKTextChoice(text: textChoiceThreeText, value: "good")
+            ORKTextChoice(text: textChoiceOneText, value: "10"),
+            ORKTextChoice(text: textChoiceTwoText, value: "9"),
+            ORKTextChoice(text: textChoiceThreeText, value: "8"),
+            ORKTextChoice(text: textChoiceFourText, value: "7"),
+            ORKTextChoice(text: textChoiceFiveText, value: "6"),
+            ORKTextChoice(text: textChoiceSixText, value: "5"),
+            ORKTextChoice(text: textChoiceSevenText, value: "4"),
+            ORKTextChoice(text: textChoiceEightText, value: "3"),
+            ORKTextChoice(text: textChoiceNineText, value: "2"),
+            ORKTextChoice(text: textChoiceTenText, value: "1"),
+            ORKTextChoice(text: textChoiceElevenText, value: "0")
         ]
         
         
         let answerFormat = ORKAnswerFormat.choiceAnswerFormatWithStyle(.SingleChoice, textChoices: textChoices)
         
-        let questionStep = ORKQuestionStep(identifier: "how do you feel this week", title: "How is your day going today?", answer: answerFormat)
+        let questionStep = ORKQuestionStep(identifier: "how do you feel this week", title: "How do you feel today?", answer: answerFormat)
+        
+        
         
         return questionStep
         
@@ -175,7 +212,15 @@ class ViewController: UIViewController, ORKTaskViewControllerDelegate{
     }
 
 
-    
+    private var summaryStep :ORKCompletionStep {
+        
+        let summaryStep = ORKCompletionStep(identifier: "SummaryStep")
+        summaryStep.title = "Right. Off you go!"
+        summaryStep.text = "That was easy!"
+        
+        return summaryStep
+    }
+
     
 
 }
