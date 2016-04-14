09b2ac780b84b9dfcd736524cbd4a50ed74c44d1
Adam Preston <adam.m.preston@gmail.com>
2016-04-14 18:56:48 -0400

    added registration and replace logo on main screen

---
+++ LoginViewController.swift
@@ -0,0 +1,70 @@
+//
+//  LoginViewController.swift
+//  workaday
+//
+//  Created by Adam Preston on 4/14/16.
+//  Copyright © 2016 RTI. All rights reserved.
+//
+
+import UIKit
+import ResearchKit
+
+class LoginViewController: UIViewController {
+
+
+    /// This tasks presents the login step.
+    private var loginTask: ORKTask {
+        /*
+         A login step view controller subclass is required in order to use the login step.
+         The subclass provides the behavior for the login step forgot password button.
+         */
+        class LoginViewController : ORKLoginStepViewController {
+            override func forgotPasswordButtonTapped() {
+                let alertTitle = NSLocalizedString("Forgot password?", comment: "")
+                let alertMessage = NSLocalizedString("Button tapped", comment: "")
+                let alert = UIAlertController(title: alertTitle, message: alertMessage, preferredStyle: UIAlertControllerStyle.Alert)
+                alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil))
+                self.presentViewController(alert, animated: true, completion: nil)
+            }
+        }
+        
+        /*
+         A login step provides a form step that is populated with email and password fields,
+         and a button for `Forgot password?`.
+         */
+        let loginTitle = NSLocalizedString("Login", comment: "")
+        let loginStep = ORKLoginStep(identifier: String("login step"), title: loginTitle, text: "Login", loginViewControllerClass: LoginViewController.self)
+        
+        /*
+         A wait step allows you to validate the data from the user login against your server before proceeding.
+         */
+        let waitTitle = NSLocalizedString("Logging in", comment: "")
+        let waitText = NSLocalizedString("Please wait while we validate your credentials", comment: "")
+        let waitStep = ORKWaitStep(identifier: String("wait_login"))
+        waitStep.title = waitTitle
+        waitStep.text = waitText
+        
+        return ORKOrderedTask(identifier: String("login stask"), steps: [loginStep, waitStep])
+    }
+    
+    
+}
+
+extension LoginViewController : ORKTaskViewControllerDelegate {
+    
+    func taskViewController(taskViewController: ORKTaskViewController, didFinishWithReason reason: ORKTaskViewControllerFinishReason, error: NSError?) {
+        switch reason {
+        case .Completed:
+            
+            // put calls to back end here
+            
+            performSegueWithIdentifier("toStudy", sender: nil)
+            
+        case .Discarded, .Failed, .Saved:
+            print("quit.")
+            dismissViewControllerAnimated(true, completion: nil)
+        }
+    }
+    
+    
+}
--- OnboardingViewController.swift
+++ OnboardingViewController.swift
@@ -30,14 +30,25 @@ class OnboardingViewController: UIViewController {
         reviewConsentStep.text = "Review the consent form."
         reviewConsentStep.reasonForConsent = "Consent to join the Developer Health Research Study."
         
-        let passcodeStep = ORKPasscodeStep(identifier: "Passcode")
-        passcodeStep.text = "Now you will create a passcode to identify yourself to the app and protect access to information you've entered."
+        let registrationTitle = NSLocalizedString("Registration", comment: "")
+        let passcodeValidationRegex = "^(?=.*\\d).{4,8}$"
+        let passcodeInvalidMessage = NSLocalizedString("A valid password must be 4 and 8 digits long and include at least one numeric character.", comment: "")
+        let registrationOptions: ORKRegistrationStepOption = [.IncludeGivenName, .IncludeFamilyName, .IncludeGender, .IncludeDOB]
+        let registrationStep = ORKRegistrationStep(identifier: String("registration_step"), title: registrationTitle, text: "Additional text can go here", passcodeValidationRegex: passcodeValidationRegex, passcodeInvalidMessage: passcodeInvalidMessage, options: registrationOptions)
+        
+        let waitTitle = NSLocalizedString("Creating account", comment: "")
+        let waitText = NSLocalizedString("Please wait while we upload your data", comment: "")
+        let waitStep = ORKWaitStep(identifier: String("waiting.step"))
+        waitStep.title = waitTitle
+        waitStep.text = waitText
+        
         
         let completionStep = ORKCompletionStep(identifier: "CompletionStep")
         completionStep.title = "Welcome aboard."
         completionStep.text = "Thank you for joining this study."
         
-        let orderedTask = ORKOrderedTask(identifier: "Join", steps: [consentStep, reviewConsentStep, passcodeStep, completionStep])
+        let orderedTask = ORKOrderedTask(identifier: "Join", steps: [consentStep, reviewConsentStep, registrationStep, waitStep,completionStep])
+        
         let taskViewController = ORKTaskViewController(task: orderedTask, taskRunUUID: nil)
         taskViewController.delegate = self
         
@@ -45,55 +56,16 @@ class OnboardingViewController: UIViewController {
     }
 }
 
-/// This task presents the Account Creation process.
-private var accountCreationTask: ORKTask {
-    /*
-     A registration step provides a form step that is populated with email and password fields.
-     If you wish to include any of the additional fields, then you can specify it through the `options` parameter.
-     */
-    let registrationTitle = NSLocalizedString("Registration", comment: "")
-    let passcodeValidationRegex = "^(?=.*\\d).{4,8}$"
-    let passcodeInvalidMessage = NSLocalizedString("A valid password must be 4 and 8 digits long and include at least one numeric character.", comment: "")
-    let registrationOptions: ORKRegistrationStepOption = [.IncludeGivenName, .IncludeFamilyName, .IncludeGender, .IncludeDOB]
-    let registrationStep = ORKRegistrationStep(identifier: String("registration_step"), title: registrationTitle, text: "Additional text can go here", passcodeValidationRegex: passcodeValidationRegex, passcodeInvalidMessage: passcodeInvalidMessage, options: registrationOptions)
-    
-    /*
-     A wait step allows you to upload the data from the user registration onto your server before presenting the verification step.
-     */
-    let waitTitle = NSLocalizedString("Creating account", comment: "")
-    let waitText = NSLocalizedString("Please wait while we upload your data", comment: "")
-    let waitStep = ORKWaitStep(identifier: String("wait_step"))
-    waitStep.title = waitTitle
-    waitStep.text = waitText
-    
-    /*
-     A verification step view controller subclass is required in order to use the verification step.
-     The subclass provides the view controller button and UI behavior by overriding the following methods.
-     */
-    class VerificationViewController : ORKVerificationStepViewController {
-        override func resendEmailButtonTapped() {
-            let alertTitle = NSLocalizedString("Resend Verification Email", comment: "")
-            let alertMessage = NSLocalizedString("Button tapped", comment: "")
-            let alert = UIAlertController(title: alertTitle, message: alertMessage, preferredStyle: UIAlertControllerStyle.Alert)
-            alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil))
-            self.presentViewController(alert, animated: true, completion: nil)
-        }
-    }
-    
-    let verificationStep = ORKVerificationStep(identifier: String("Verification Text d"), text: "verification text can go here.", verificationViewControllerClass: VerificationViewController.self)
-    
-    return ORKOrderedTask(identifier: String("Account Creation Task"), steps: [
-        registrationStep,
-        waitStep,
-        verificationStep
-        ])
-}
+
 
 extension OnboardingViewController : ORKTaskViewControllerDelegate {
     
     func taskViewController(taskViewController: ORKTaskViewController, didFinishWithReason reason: ORKTaskViewControllerFinishReason, error: NSError?) {
         switch reason {
         case .Completed:
+            
+            // put calls to back end here
+            
             performSegueWithIdentifier("unwindToStudy", sender: nil)
             
         case .Discarded, .Failed, .Saved:
