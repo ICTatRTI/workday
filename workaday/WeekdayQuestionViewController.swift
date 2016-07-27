702e9bfbc2c48dd03d2556f1c38136530f97b768
Adam Preston <adam.m.preston@gmail.com>
2016-07-27 16:50:25 -0600

    added library for implementing a radio buttom

---
+++ workaday/WeekdayQuestionViewController.swift
@@ -0,0 +1,52 @@
+//
+//  WeekdayQuestionViewController.swift
+//  workaday
+//
+//  Created by Adam Preston on 7/27/16.
+//  Copyright © 2016 RTI. All rights reserved.
+//
+
+import UIKit
+
+
+class WeekdayQuestionViewController: UIViewController, SSRadioButtonControllerDelegate {
+    
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
+    @IBOutlet weak var button4: UIButton!
+    @IBOutlet weak var button5: UIButton!
+    @IBOutlet weak var button6: UIButton!
+    
+    @IBOutlet weak var doneButton: UIButton!
+    
+    var radioButtonController: SSRadioButtonsController?
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        radioButtonController = SSRadioButtonsController(buttons: button1, button2, button3, button4, button5, button6)
+        radioButtonController!.delegate = self
+        radioButtonController!.shouldLetDeSelect = true
+        
+        // Style the done button (simliar to RK)
+        doneButton.backgroundColor = UIColor.clearColor()
+        doneButton.layer.cornerRadius = 5
+        doneButton.layer.borderWidth = 1
+        doneButton.contentEdgeInsets = UIEdgeInsetsMake(10,20,10,20)
+        
+        let swiftColor = UIColor(red: 0, green: 122/255, blue: 1, alpha: 1)
+        doneButton.layer.borderColor = swiftColor.CGColor
+            
+        
+        print("loading Quesionaire")
+    }
+
+    func didSelectButton(aButton: UIButton?) {
+        print(aButton)
+    }
+    
+    
+}
+
---
+++ workaday/WeekendQuestionViewController.swift
@@ -0,0 +1,23 @@
+//
+//  WeekendQuestionViewController.swift
+//  workaday
+//
+//  Created by Adam Preston on 7/27/16.
+//  Copyright © 2016 RTI. All rights reserved.
+//
+
+import UIKit
+
+class WeekendQuestionViewController: UIViewController {
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("loading Quesionaire")
+    }
+    
+    
+    
+}
